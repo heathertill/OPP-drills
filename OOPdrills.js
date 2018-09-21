a9103dc0bc3e5950f8348edec7692d79ff91acbf
heathertill <heather.till@charter.net>
2018-09-21 16:12:36 -0500

    object literals created

---
+++ OOPdrills.js
@@ -0,0 +1,51 @@
+let person1 = {
+    name: {
+        first: 'Kenneth',
+        last: 'Till',
+    },
+    greeting: function sayHello() {
+        confirm.log(`Hello! My name is ${first} ${last}.`)
+    }
+}
+
+let person1 = {
+    name: {
+        first: 'Caroline',
+        last: 'Till',
+    },
+    greeting: function sayHello() {
+        confirm.log(`Hello! My name is ${first} ${last}.`)
+    }
+}
+
+let person1 = {
+    name: {
+        first: 'Reid',
+        last: 'Till',
+    },
+    greeting: function sayHello() {
+        confirm.log(`Hello! My name is ${first} ${last}.`)
+    }
+}
+
+let person1 = {
+    name: {
+        first: 'Davis',
+        last: 'Till',
+    },
+    greeting: function sayHello() {
+        confirm.log(`Hello! My name is ${first} ${last}.`)
+    }
+}
+
+let person1 = {
+    name: {
+        first: 'Margaret',
+        last: 'Waldrop',
+    },
+    greeting: function sayHello() {
+        confirm.log(`Hello! My name is ${first} ${last}.`)
+    }
+}
+
+person1.greeting()
